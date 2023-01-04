293af416be78869e361990c62cbdfe1e54e2c572
PokkeYuri <86960788+PokkeYuri@users.noreply.github.com>
2023-01-04 21:28:25 +0100

    +Maze Solver

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from tkinter import Tk, BOTH, Canvas
 import time
 import random
 
+WINDOW_HEIGHT = 800
+WINDOW_WIDHT =800
+NUM_COLUMNS = 30
+NUM_ROWS = 30
+CELL_SIZE = 20
 SEARCHPATTERN = {
     "N":(-1, 0),
     "S":(1, 0),
@@ -10,23 +15,9 @@ SEARCHPATTERN = {
 }
 
 def main():
-    win = Window(800, 800)
-    #point1 = Point(10, 10)
-    #point2 = Point(40, 40)
-    #point3 = Point(40, 10)
-    #point4 = Point(70, 40)
-    #line = Line(point1, point2)
-    #win.draw_line(line, "black")
-    #cell = Cell(point1, point2)
-    #cell2 = Cell(point3, point4)
-    
-    #cell.draw(win, "black") 
-    #cell2.draw(win, "black")
-
-    #cell.draw_move(win, cell2)
-    num_cols = 30
-    num_rows = 30
-    m1 = Maze(Point(10,10), num_rows, num_cols, 20, 20, win)
+    win = Window(WINDOW_HEIGHT, WINDOW_WIDHT)
+    m1 = Maze(Point(10,10), NUM_ROWS, NUM_COLUMNS, CELL_SIZE, CELL_SIZE, win)
+    m1.run(win)
     win.wait_for_close()
 
 class Point:
@@ -38,6 +29,7 @@ class Line:
     def __init__(self, point1: Point, point2: Point) -> None:
         self.point1 = point1
         self.point2 = point2
+
     def draw(self, canvas: Canvas, fill_color: str) -> None:
         canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
         canvas.pack()
@@ -51,15 +43,19 @@ class Window:
         self.canvas.pack()
         self.run = False
         self.win.protocol("WM_DELETE_WINDOW", self.close)
+
     def redraw(self) -> None:
         self.win.update_idletasks()
         self.win.update()   
+
     def draw_line(self, line: Line, fill_color: str):
         line.draw(self.canvas, fill_color)
-    def wait_for_close(self) -> None:
+
+    def wait_for_close(self) -> bool:
         self.run = True
         while self.run:
             self.redraw()
+
     def close(self) -> None:
         self.run = False
 
@@ -74,9 +70,11 @@ class Cell:
         self.visited = False
         self.maze_x = maze_x
         self.maze_y = maze_y
-    def __get_center_point(self) -> Point:
+
+    def get_center_point(self) -> Point:
         point = Point(self.point1.x + abs(self.point1.x - self.point2.x)/2,self.point1.y + abs(self.point1.y - self.point2.y) /2)
         return point
+
     def draw(self, window: Window, color: str) -> None:
         if self.has_top_wall:
             window.draw_line(Line(point1=self.point1, point2=Point(self.point2.x, self.point1.y)), color)
@@ -86,12 +84,14 @@ class Cell:
             window.draw_line(Line(point1=self.point2, point2=Point(self.point1.x, self.point2.y)), color)
         if self.has_left_wall:
             window.draw_line(Line(point1=Point(self.point1.x, self.point2.y), point2=self.point1), color)
+
     def draw_move(self,window:Window, to_cell, undo=False) -> None:
         color = "red"
         if undo:
             color = "gray"
-        window.draw_line(Line(self.__get_center_point(), to_cell.__get_center_point()), color)
-    def remove_wall_to_cell(self, cell):
+        window.draw_line(Line(self.get_center_point(), to_cell.get_center_point()), color)
+
+    def remove_wall_to_cell(self, cell) -> None:
         delta_x, delta_y =  cell.maze_x - self.maze_x, cell.maze_y - self.maze_y
         direction = None
         for key, value in SEARCHPATTERN.items():
@@ -112,20 +112,23 @@ class Cell:
         
 
 class Maze:
-    def __init__(self, point: Point, num_rows: int, num_cols: int, cell_size_x: int, cell_size_y: int, win: Window, seed :int = None) -> None:
+    def __init__(self, point: Point, num_rows: int, num_cols: int, cell_size_x: int, cell_size_y: int, seed :int = None) -> None:
         self.point = point
         self.num_rows = num_rows
         self.num_cols = num_cols
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
-        self.win = win
-        
         self._cells = []
-        self._create_cells()
-        self._break_entrance_and_exit()
-        self._break_walls_r()
-        self._draw_cell()
-    def _create_cells(self):
+
+    def run(self, win: Window)  -> None:
+        self.create_cells()
+        self.break_entrance_and_exit()
+        self.break_walls()
+        self.draw_cell(win)
+        self.reset_cell_visited()
+        self.solve(win)
+
+    def create_cells(self)  -> None:
         current = self.point
         for i in range(self.num_cols):
             cell_row = []
@@ -134,18 +137,24 @@ class Maze:
                 current = Point(current.x + self.cell_size_x,current.y)
             self._cells.append(cell_row)
             current = Point(self.point.x, self.point.y + (self.cell_size_y * (i+1))) 
-    def _draw_cell(self):
+
+    def draw_cell(self, win: Window) -> None:
+        win.run = True
         for row in self._cells:
+            if not win.run:
+                break; 
             for cell in row:
-                cell.draw(self.win, "black")
-                self._animate()
-    def _animate(self):
-        self.win.redraw()
-        time.sleep(0.05)
-    def _break_entrance_and_exit(self):
+                if not win.run:
+                    break; 
+                cell.draw(win, "black")
+                win.redraw()
+                #time.sleep(0.05)
+
+    def break_entrance_and_exit(self) -> None:
         self._cells[0][0].has_left_wall = False
         self._cells[-1][-1].has_right_wall = False
-    def _find_cell_neighbour(self, column:int , row:int):
+
+    def find_cell_neighbour(self, column:int , row:int) -> list:
         neighbours = []
         for value in SEARCHPATTERN.values():
             y = column + value[0]
@@ -154,9 +163,9 @@ class Maze:
                 continue
             if self._cells[y][x].visited == False:
                 neighbours.append(self._cells[y][x])
-
         return neighbours
-    def _break_walls_r(self):
+
+    def break_walls(self) -> None:
         self._cells[0][0].visited = True
         cell_stack = []
         cell_stack.append(self._cells[0][0])
@@ -164,14 +173,61 @@ class Maze:
         current = None
         while len(cell_stack) > 0:
             current = cell_stack.pop()
-            neighbours = self._find_cell_neighbour(current.maze_y, current.maze_x)
+            neighbours = self.find_cell_neighbour(current.maze_y, current.maze_x)
             if len(neighbours) > 0:
                 cell_stack.append(current)
                 chosen = random.choice(neighbours)
-                #chosen = neighbours[0]
                 chosen.visited = True
                 current.remove_wall_to_cell(chosen)               
                 cell_stack.append(chosen)
 
+    def reset_cell_visited(self) -> None:
+        for row in self._cells:
+            for cell in row:
+                cell.visited = False
+
+    def solve(self, win:Window) -> bool:
+        self._cells[0][0].visited = True
+        cell_stack = []
+        cell_stack.append(self._cells[0][0])
+        current = None
+        while len(cell_stack) > 0:
+            current = cell_stack.pop()
+            neighbours = self.find_path_neighbour(current.maze_y, current.maze_x)
+            if len(neighbours) > 0:
+                cell_stack.append(current)
+                chosen = random.choice(neighbours)
+                chosen.visited = True 
+                win.draw_line(Line(current.get_center_point(), chosen.get_center_point()), "red")       
+                win.redraw()
+                if chosen == self._cells[-1][-1]:
+                    return True      
+                time.sleep(0.1)
+                cell_stack.append(chosen)
+        return False
+
+    def find_path_neighbour(self, column:int, row:int) -> list:
+        neighbours = []
+        for key, value in SEARCHPATTERN.items():
+            next_column = column + value[0]
+            next_row = row + value[1]
+            if next_row < 0 or next_column < 0 or next_column > (self.num_cols-1) or next_row > (self.num_rows-1):
+                continue
+            ok = False
+            if key == "N" and not self._cells[column][row].has_top_wall:
+                if self._cells[next_column][next_row].visited == False:
+                    ok = True
+            elif key == "S" and not self._cells[column][row].has_bottom_wall:
+                if self._cells[next_column][next_row].visited == False:
+                    ok = True
+            elif key == "W" and not self._cells[column][row].has_left_wall:
+                if self._cells[next_column][next_row].visited == False:
+                    ok = True
+            elif key == "O" and not self._cells[column][row].has_right_wall:
+                if self._cells[next_column][next_row].visited == False:
+                    ok = True
+            if ok:
+                neighbours.append(self._cells[next_column][next_row])
+        return neighbours
 
 main()
